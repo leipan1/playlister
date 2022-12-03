c965557054a80a3f4ad55453aa223666bb14341e
Leila Pan <leila.pan@stonybrook.edu>
2022-12-03 13:48:36 -0500

    sort menu added

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,9 +1,46 @@
+import { useContext, useState } from 'react';
+
 import HomeIcon from '@mui/icons-material/Home';
 import GroupsIcon from '@mui/icons-material/Groups';
 import PersonIcon from '@mui/icons-material/Person';
 import SortIcon from '@mui/icons-material/Sort';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 export default function MenuBar(){
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const isMenuOpen = Boolean(anchorEl);
+
+    const handleSortMenuOpen=(event)=>{
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const sortMenu= 
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem>Name(A-Z)</MenuItem>
+            <MenuItem>Publish Date(Newest)</MenuItem>
+            <MenuItem>Listens(High-Low)</MenuItem>
+            <MenuItem>Likes(High-Low)</MenuItem>
+            <MenuItem>Dislikes(High-Low)</MenuItem>
+        </Menu>
 
     return(
         <div id="menu-bar">
@@ -21,10 +58,16 @@ export default function MenuBar(){
             <input id="menu-bar-search-bar" type="text" placeholder="Search"/>
             <div id="menu-bar-sort">
                 <p style={{ fontSize: '15pt', marginRight:'10px'}}>SORT BY</p>
-                <SortIcon
-                    fontSize='inherit'
-                />
+                <div id="menu-bar-sort-icon">
+                    <SortIcon
+                        fontSize='inherit'
+                        onClick={handleSortMenuOpen}
+                    />
+                </div>
             </div>
+            {
+                sortMenu
+            }
         </div>
     );
 }
